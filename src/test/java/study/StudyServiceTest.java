28402d4a8305cbcea97b190a909575ae39e7b4d3
KIM <ehddnr0501@gmail.com>
2022-10-10 21:59:46 +0900

    temp

--- src/test/java/study/StudyServiceTest.java
+++ src/test/java/study/StudyServiceTest.java
@@ -2,69 +2,98 @@ package study;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 import java.util.Optional;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import domain.Member;
 import domain.Study;
 import member.MemberService;
 
-@ExtendWith(MockitoExtension.class)//Mock 생성 - 확장
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@ActiveProfiles("test")
+@Testcontainers
 class StudyServiceTest {
-	
-	@Mock
-	MemberService memberService;
-	
-	@Mock
-	StudyRepository studyRepository;
-	
-	@Test
-	@DisplayName("Mockito BDD 스타일 API")
-    void createNewStudy(@Mock MemberService memberService,
-                            @Mock StudyRepository studyRepository) {
-		// Given
+
+    @Mock MemberService memberService;
+
+    @Autowired StudyRepository studyRepository;
+
+//    @Container
+//    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer<>()
+//            .withDatabaseName("studytest");
+    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer();
+    
+//    @BeforeAll
+//    static void beforeAll () {
+//    	postgreSQLContainer.start();
+//    	System.out.println(postgreSQLContainer.getJdbcUrl());
+//    }
+//    
+//    @AfterAll
+//    static void afterAll() {
+//    	postgreSQLContainer.stop();
+//    }
+
+    @BeforeEach
+    void beforeEach() {
+        studyRepository.deleteAll();
+    }
+
+    @Test
+    void createNewStudy() {
+        // Given
         StudyService studyService = new StudyService(memberService, studyRepository);
-        //Test할 객체 생성 
         assertNotNull(studyService);
 
         Member member = new Member();
         member.setId(1L);
         member.setEmail("keesun@email.com");
-        
-        Study study = new Study(10, "java");
-        
-        //Stubbing
-//        when(memberService.findById(1L)).thenReturn(Optional.of(member));
-        //any() => 모든 값 대응
-//        when(memberService.findById(any())).thenReturn(Optional.of(member));
-//        when(studyRepository.save(study)).thenReturn(study);
-        
-//        import static
+
+        Study study = new Study(10, "테스트");
+
         given(memberService.findById(1L)).willReturn(Optional.of(member));
-        given(studyRepository.save(study)).willReturn(study);
-        
+
         // When
-        //notify() 한번 실행
         studyService.createNewStudy(1L, study);
-        
+
         // Then
-        assertEquals(member, study.getOwner());
-        //memberService의 notify(study) method가 1번 호출 되어야 한다. test
-//        verify(memberService, times(1)).notify(study);
+        //assertEquals(1L, study.getOwnerId());
         then(memberService).should(times(1)).notify(study);
-        //어떠한 액션 이후에 더 이상 객체를 사용하지 않아야 함.
-//        verifyNoInteractions(memberService);
-        then(memberService).shouldHaveNoInteractions();
-        
+        then(memberService).shouldHaveNoMoreInteractions();
     }
+
+    @DisplayName("다른 사용자가 볼 수 있도록 스터디를 공개한다.")
+    @Test
+    void openStudy() {
+        // Given
+        StudyService studyService = new StudyService(memberService, studyRepository);
+        Study study = new Study(10, "더 자바, 테스트");
+        assertNull(study.getOpenedDateTime());
+
+        // When
+        studyService.openStudy(study);
+
+        // Then
+        assertEquals(StudyStatus.OPENED, study.getStatus());
+        assertNotNull(study.getOpenedDateTime());
+        then(memberService).should().notify(study);
+    }
+
 }
